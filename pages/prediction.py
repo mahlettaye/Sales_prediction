3d2cb28a0ac12e93b180d5fdbf2b052c04e1c5b2
mahlettaye <you@example.com>
2021-08-28 11:40:18 +0300

    streamlit is added

---
+++ pages/prediction.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+import sys
+sys.path.insert(0, '/home/mahlet/10ac/Sales_prediction/')
+
+
+
+def read_csv (filename):
+    
+        try:
+            data= pd.read_csv(filename)
+        
+        except FileNotFoundError as e:
+            print("unable to open")
+        
+        return data
+
+def load(scaler_path, model_path):
+    sc = joblib.load(scaler_path)
+    model = joblib.load(model_path)
+    return sc , model
+
+def inference(scaler, model, df):
+    df=df.drop(['Unnamed: 0'], axis=1)
+    df=df[['Customers','StoreType','CompetitionDistance','Store','Promo','Promo2SinceWeek']]
+        #df = pd.DataFrame([row], columns = feat_cols)
+
+    #X = scaler.transform(df)
+        #features = pd.DataFrame(X)
+    prediction=model.predict(df)
+    return prediction
+            
+       
+
+   
+
+def app ():
+
+    with st.spinner("Loading"):
+        test_file= st.file_uploader('Upload File', type=['csv'])
+
+        if (test_file):
+            test=read_csv(test_file)
+            st.dataframe(test.head(20))
+            sample=test[:5]
+            if (st.button('Predict')):
+                model = load('rfmodel.joblib')
+                result = inference(model, sample)
+               
+                st.dataframe(result)
+            
+   
+
